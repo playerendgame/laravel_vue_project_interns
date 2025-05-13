41e0edc02f6d3186c8662c3162fc62caf4d9e5c4
Michael <dulaogonmichael_act@plmun.edu.ph>
2025-05-13 13:21:14 +0800

    Commit

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,41 +6,10 @@
 <body>
 
     <div class="container-fluid">
-        {{-- @yield('contents') --}}
+        @yield('contents') 
 
-        <div class="wholeskull">
-            <div class="skull">
-            <div class="top">
-                <div class="horn"></div>
-            </div>
-            <div class="bottom">
-                <div class="white"></div>
-                <div class="teeth">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                </div>
-            </div>
-            <div class="eyes">
-                <div class="iris">
-                <div class="eyeball">
-
-                </div>
-                </div>
-            </div>
-
-            </div>
-            <div class="mouth"></div>
-        </div>
-        </div>
-        <div class="shadow"></div>
     </div>
-    
+        
     @include('script')
 
 
