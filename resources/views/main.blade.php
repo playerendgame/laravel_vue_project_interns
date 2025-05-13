1ffa1a651b6c33716d233fd3ca4d47170e469e6d
playerendgame <148306699+playerendgame@users.noreply.github.com>
2025-05-13 13:06:44 +0800

    test

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,39 @@
 <body>
 
     <div class="container-fluid">
-        @yield('contents')
+        {{-- @yield('contents') --}}
+
+        <div class="wholeskull">
+            <div class="skull">
+            <div class="top">
+                <div class="horn"></div>
+            </div>
+            <div class="bottom">
+                <div class="white"></div>
+                <div class="teeth">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                </div>
+            </div>
+            <div class="eyes">
+                <div class="iris">
+                <div class="eyeball">
+
+                </div>
+                </div>
+            </div>
+
+            </div>
+            <div class="mouth"></div>
+        </div>
+        </div>
+        <div class="shadow"></div>
     </div>
     
     @include('script')
