93fc9cdbfb05747876ccc57bec0b33aca2648702
krystal422 <tarunronroa.2@gmail.com>
2025-05-14 12:00:02 +0800

    categories_content

--- resources/js/components/LandingPage/CategoriesComponent.vue
+++ resources/js/components/LandingPage/CategoriesComponent.vue
@@ -1,7 +1,59 @@
 <template>
 
     <div>
-        Test
+    
+        <div class="container-fluid CarouselContainer">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="https://img.freepik.com/free-photo/mesmerizing-scenery-green-mountains-with-cloudy-sky-surface_181624-27189.jpg">
+                </div>
+                <div class="col-md-6">
+                    <img src="https://img.freepik.com/free-photo/mesmerizing-scenery-green-mountains-with-cloudy-sky-surface_181624-27189.jpg">
+                </div>
+            </div>
+            <div class="row text-center">
+                text text text text text
+            </div>
+        </div>
+        <hr>
+        <div class="container-fluid WhyTravel">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Why Travel?</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                <h2>Adventure awaits around every corner—pack your bags, chase the laughter, and let the world be your playground.</h2>
+                </div>
+                <div class="col-md-6">
+                    <img src="https://img.freepik.com/free-photo/asian-young-happy-family-enjoy-vacation-beach-evening-dad-mom-kid-relax-playing-together-near-sea-when-silhouette-sunset-lifestyle-travel-holiday-vacation-summer-concept_7861-2443.jpg">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="https://img.freepik.com/free-photo/young-family-together-walking-forest-winter-time_1303-20135.jpg">
+                </div>
+                <div class="col-md-6">
+                    <img src="https://img.freepik.com/free-photo/mom-dad-daughter_1157-4281.jpg">
+                </div>
+            </div>
+        </div>
+        <hr>
+
+        <div class="container-fluid TravelTips">
+            <div class="row align-items-center">
+                <div class="col-md-12">
+                    <p>1. Plan—but Stay Flexible. Research and plan you itinerary and strategies.</p>
+                    <p>2. Organize Smart. Prepare your fits and things ahead of time!</p>
+                    <p>3. Prepare your Tech. Scan passports, download Maps, and share info.</p>
+                    <p>4. Embrace Slow Travel. Enjoy and take your time to make the best out of traveling.</p>
+                </div>
+            </div>
+            <div class="row text-center">
+                <p>Use these tips as a foundation, then tweak them to your own style—whether you’re a solo adventurer, a family group, or friends on a road trip. Safe travels and happy exploring!</p>
+            </div>
+        </div>
     </div>
 
 </template>
